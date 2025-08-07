032de00e112c457b713113845dea2fe2a2562639
kamaukimani <paulkimys285@gmail.com>
2025-08-07 10:04:07 +0300

    add file and its content

---
+++ classes/intro.py
@@ -0,0 +1,20 @@
+# class Name:First letter Cap<>
+class Student:
+    name="Paul"
+    email="paul@gmail.com"
+    phone="254712345678"
+
+
+    #students
+student1=Student() #Object datatype: dot notation
+student2=Student()
+# student 1
+print("student 1")
+print("name",student1.name)
+print("email",student1.email)
+print("phone",student1.phone)
+#student 2
+print("student 2")
+print("name",student2.name)
+print("email",student2.email)
+print("phone",student2.phone)
