2dd39b7965d6bf0ba722723c5b891437394056d7
kamaukimani <paulkimys285@gmail.com>
2025-08-18 09:17:06 +0300

    add file and its content

---
+++ dbs/post/alter_table.sql
@@ -0,0 +1,44 @@
+--ALTER STATEMENT
+-- ALTER TABLE <table> <instruction<add, drop> <column>>
+-- Common Sense
+-- ALTER TABLE student 
+-- ADD 
+-- pocket_money INTEGER;
+-- DEFAULT VALUE or MAKE IT NULL
+-- UNIQUE
+
+-- ALTER IN STEPS
+-- CREATE MAKE IT NULL
+
+-- ALTER TABLE student 
+-- ALTER COLUMN pocket_money SET NOT NULL;
+
+-- ADD THE DATA
+-- ALTER STAMENT TO CONSTRAIN THE COLUMN
+
+-- ALTER TABLE student drop
+-- COLUMN is_married;
+
+-- COMMON SENSE
+--
+
+---aloo---
+-- ALTER TABLE student
+-- ADD
+-- pocket_money INTEGER;
+
+-- ALTER TABLE student
+-- ALTER COLUMN pocket_money SET NOT NULL;
+-- ALTER TABLE student
+-- ADD
+-- have_father BOOLEAN ;
+
+-- ALTER TABLE student
+-- ALTER COLUMN have_father SET NOT NULL;
+
+-- ALTER TABLE student
+-- ADD 
+-- delete_this INTEGER;
+
+-- ALTER TABLE student DROP 
+-- COLUMN delete_this;
---
+++ dbs/post/constraints.sql
@@ -0,0 +1,25 @@
+--DROP TABLE IF EXISTS student;
+CREATE TABLE IF NOT EXISTS student(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    email  VARCHAR(20) NOT NULL UNIQUE,
+    dob DATE,
+    phone INTEGER NOT NULL UNIQUE,
+    marks REAL CHECK(marks > -1 AND marks < 101),
+    is_married BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- CREATE TABLE IF NOT EXISTS student(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(20) NOT NULL,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     dob DATE,
+--     phone INTEGER NOT NULL UNIQUE,
+--     marks REAL CHECK(marks>-1 AND marks<101),
+--     pocket_money INTEGER,
+--     is_married BOOLEAN NOT NULL DEFAULT FALSE,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- -- TEXT -> holw book
+-- -- VARCHAR(50)
